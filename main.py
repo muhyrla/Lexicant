26d681bba77d45c00c7b6395eb8b3e51785af4b5
Mikhail <78497404+muhyrla@users.noreply.github.com>
2024-04-14 14:47:22 +0600

    v6 beta
    - добавили босса
    - стабильнее работает какбудто
    - поменяли шрифт на читаемый
    - переделали игру на русский язык

--- main.py
+++ main.py
@@ -61,14 +61,19 @@ max_flying_monster_height = screen_height - player_height - 300
 
 monsters = []
 particles = []
-words_to_use = r.random_words(200, word_max_length=3)
 
-font = pygame.font.Font('src/Minecraft.ttf', 36)
+with open('src/summary.txt', encoding='utf-8') as words:
+    words_to_use = words.readlines()
+
+font = pygame.font.Font('src/Hardpixel.otf', 36)
 
 monster_images = {
     "wisp": pygame.transform.scale(pygame.image.load("src/wisp.png").convert_alpha(), (100, 100)),
     "slime": pygame.transform.scale(pygame.image.load("src/slime.png").convert_alpha(), (80, 80))
 }
+
+monster_images['ponasennkov'] = pygame.transform.scale(pygame.image.load("src/ponasenkov.jpg").convert_alpha(), (200, 200))
+
 
 full_heart_image = pygame.image.load("src/full_heart.png").convert_alpha()
 full_heart_image = pygame.transform.scale(full_heart_image, (80, 80))
@@ -98,13 +103,19 @@ class Particle:
 
 
 def create_monster():
+
+    if any(m[2] == 'ponasennkov' for m in monsters):
+        return
+
     monster_type = random.choice(["wisp", "slime"])
     monster_image = monster_images[monster_type]
     monster_width = monster_image.get_width()
     monster_height = monster_image.get_height()
     monster_x = screen_width
-    monster_word = random.choice(words_to_use)
-
+    word_length = 100
+    while word_length >= 5:
+        monster_word = random.choice(words_to_use).replace('\n', '')
+        word_length = len(monster_word)
     if monster_type == "wisp":
         monster_y = random.randint(50, max_flying_monster_height)
     else:
@@ -118,26 +129,30 @@ def create_monster():
 
 
 def update_monsters():
-    global player_health
-    levitation_range = 10
-    levitation_speed = 0.05
+    global player_health, monsters
 
-    for i in range(len(monsters) - 1, -1, -1):
-        monster, word, monster_type, angle, base_y = monsters[i]
-        monster_speed = 3 if monster_type == "wisp" else 5
-        monster.x -= monster_speed
+    for i, (monster, word, monster_type, angle, base_y) in enumerate(monsters):
+        if monster_type == 'ponasennkov':
+            monster.x -= 1
+            if monster.x <= player.x + player.width:
+                game_over()
+                return
+            if word == "":
+                monsters.pop(i)
+                continue
 
-        if monster.right < 0:
-            monsters.pop(i)
-            player_health -= 1
-        elif monster_type == "wisp":
-            angle += levitation_speed
+        else:
+            monster.x -= 3
+            if monster.right < 0:
+                monsters.pop(i)  
+                if monster_type != 'ponasennkov':
+                    player_health -= 1
+
+        if monster_type in ['wisp']:
+            angle += 0.05
+            levitation_range = 10
             monster.y = base_y + levitation_range * math.sin(angle)
             monsters[i] = (monster, word, monster_type, angle, base_y)
-
-    if player_health <= 0:
-        game_over()
-
 
 
 
@@ -188,12 +203,13 @@ def reset_game():
     player_health = 3
     player_score = 0
     x_pos = 0
+    boss_appeared = False
 
     monsters = []
 
 
 def show_menu():
-    title_text = font.render("Lexicant, the Word Wizard", True, BLACK)
+    title_text = font.render("Lexicat, the Word Wizard", True, BLACK)
     play_button = pygame.Rect(0, 0, 200, 80)
     play_button.center = (900//2+180, 900//2)
 
@@ -220,9 +236,13 @@ def main():
 
     reset_game()
     show_menu()
+    boss_appeared = False
     monster_timer = 0
     user_input = ""
     clock = pygame.time.Clock()
+    boss_appeared = False
+    boss_phrase = "всей швали моих критиков моих завистников вы думаете что с человеком который вот до такой степени точно исследует тему можно спорить вы думаете что я вас не переиграю что я вас не уничтожу я вас уничтожу".split()
+    current_boss_word_index = 0
 
     running = True
     
@@ -233,7 +253,7 @@ def main():
         if x_pos < 0:
             screen.blit(background_image, (x_pos + image_width, 0))
         
-        x_pos -= 2
+        x_pos -= 2.2
         
         if -x_pos >= image_width:
             x_pos = 0
@@ -242,21 +262,43 @@ def main():
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
-                    for i, (monster, word, monster_type, angle, base_y) in enumerate(monsters):
-                        if word == user_input:
-                            monsters.pop(i)
-                            create_particles(monster.x, monster.y)
-                            player_score += 500
-                            break
-                    user_input = ""
+                    if boss_appeared and monsters[0][2] == 'ponasennkov':
+                        boss, word, boss_type, angle, base_y = monsters[0]
+                        if user_input.strip() == word:
+                            current_boss_word_index += 1
+                            if current_boss_word_index >= len(boss_phrase):
+                                monsters.pop(0)
+                                boss_appeared = False
+                            else:
+                                monsters[0] = (boss, boss_phrase[current_boss_word_index], boss_type, angle, base_y)
+                            user_input = ""
+                        else:
+                            pass
+                    else:
+                        for i, (monster, word, monster_type, angle, base_y) in enumerate(monsters):
+                            if word == user_input:
+                                create_particles(monster.x, monster.y)
+                                monsters.pop(i)
+                                player_score += 9000
+                                break
+                        user_input = ""
                 elif event.key == pygame.K_BACKSPACE:
                     user_input = user_input[:-1]
                 elif event.key is pygame.K_ESCAPE:
                     pygame.quit()
                     quit()
                 else:
-                    if 'a' <= event.unicode <= 'z' or 'A' <= event.unicode <= 'Z':
+                    if 'а' <= event.unicode <= 'я' or 'А' <= event.unicode <= 'Я':
                         user_input += event.unicode.lower()
+
+        if player_score >= 10000 and not boss_appeared:
+            monsters.clear()  
+            boss_x = screen_width - 200
+            boss_y = screen_height // 2 - 100
+            boss = pygame.Rect(boss_x, boss_y, 200, 200)
+            monsters.append((boss, boss_phrase[current_boss_word_index], 'ponasennkov', 0, boss_y))
+            boss_appeared = True
+
 
         monster_timer += 1
         if monster_timer >= 100:  
@@ -283,6 +325,7 @@ def main():
             screen.blit(monster_images[monster_type], (monster.x, monster.y))
             word_text = font.render(word, True, BLACK)
             screen.blit(word_text, (monster.centerx - word_text.get_width() // 2, monster.y - word_text.get_height() - 10))
+
 
         for i in range(player_health):
             screen.blit(full_heart_image, (10 + i * 90, 10))
